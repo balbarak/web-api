4603e2b28e5d0cd0c7977b09b8c2a6e43af9dc5f
Bader Albarrak <balbarak@live.com>
2017-10-01 19:58:25 +0300

    sort files

---
+++ src/Controllers/DateController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DateWebApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DateWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class DateController : Controller
+    {
+        // GET: api/date
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Date date = new Date();
+
+            return Ok(date);
+        }
+        
+    }
+}
---
+++ src/Models/Date.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateWebApi.Models
+{
+    public class Date
+    {
+        public string GregorianDate { get; set; }
+
+        public string HijriDate { get; set; }
+
+        public string HijriDateFull { get; set; }
+
+        public Date()
+        {
+            GregorianDate = DateTime.Now.ToString("dd-MM-yyyy", new CultureInfo("en-US"));
+
+            HijriDate = DateTime.Now.ToString("dd-MM-yyyy", new CultureInfo("ar-SA"));
+
+            HijriDateFull = DateTime.Now.ToString("dddd, dd MMMM, yyyy هـ", new CultureInfo("ar-SA"));
+            
+        }
+    }
+}
